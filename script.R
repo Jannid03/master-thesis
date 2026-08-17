a90288bd178c5f63cd066967ba98a670b17562f1
Jannid03 <jannikdubrau@gmx.de>
2026-08-17 22:02:06 +0200

    Analysis of 100 runs for each variance

--- script.R
+++ script.R
@@ -4,56 +4,105 @@ setup()
 
 ## For base simulation
 # init("base",42)
-init("tmax",4860,sqrt(0.1)) #12
-run()
+# init("tmax",4870,sqrt(0.1)) #12
+# run()
+# for (i in 1:100) {
+#   init("tmax",i,sqrt(0.001)) #12
+#   run(n=TRUE,i)
+# }
 
-# load(parameter = "tmax", runn = 48)
-value_df <- load()
-standard_plots(value_df)
-# standard_plots(value_df,27)
+dfs <- c()
+for (i in 1:300){
+  print(i)
+  value_df <- load(parameter = "tmax", runn = i)
 
-###### Extra Plots
-### all cells plotted
-all_cells(value_df)
-all_cells(value_df,"eTNFa")
-all_cells(value_df, "activated_frac")
+  dfs <- c(dfs, value_df)
+}
 
-### maxima NFKB
-maxima(value_df)
-maxima(value_df,"eTNFa")
-maxima(value_df,"activated_frac")
+vals <- c()
+for (i in 0:299){
+  print(i)
+  value_df <- dfs[3*i+1]
+  
+  
+  ### Avergae of first time point vs. average of last time point
+  # first <- value_df$df |> group_by(cell.id )|> filter(time == -1) |> select(NFKB.n,cell.id)
+  # end <- value_df$df |> group_by(cell.id )|> filter(time == 800) |> select(NFKB.n,cell.id)
+  # 
+  # vals <- c(vals, mean(end[["NFKB.n"]])/mean(first[["NFKB.n"]]))
+  
+  max_pre <- value_df$df |> group_by(cell.id) |> filter(time <= -1) |> slice_max(NFKB.n)
+  
+  max_post <- value_df$df |> group_by(cell.id) |> filter(time > -1) |> slice_max(NFKB.n)
+  
+  vals <- c(vals, sum(max_pre[["NFKB.n"]]*0.9 > max_post[["NFKB.n"]]))
+  
+}
 
-##Kymographs
-kymograph(value_df)
-kymograph(value_df,"eTNFa")
+# results <- data.frame("var"=as.factor(rep(c(0.001,0.01,0.1),each=100)),"average_rise"=vals)
 
-###AUCS
-auc_plot(value_df)
-auc_plot(value_df,"eTNFa")
+results <- cbind(results,data.frame(""=vals))
 
-### Response times
-response_plot(value_df)
-response_plot(value_df,"eTNFa")
+ggplot(data=results)+
+  geom_boxplot(mapping=aes(x=(var),y=average_rise))
+
+ggplot(data=results)+
+  geom_boxplot(mapping=aes(x=var,y=non_activated_premax))
+# 
+# plot(1:100,ends)
+# value_df <- load(parameter = "tmax", runn = 241)
+# value_df <- load()
+# standard_plots(value_df)
+# # standard_plots(value_df,27)
+# 
+# ###### Extra Plots
+# ### all cells plotted
+# all_cells(value_df)
+# all_cells(value_df,"eTNFa")
+# all_cells(value_df, "activated_frac")
+# 
+# ### maxima NFKB
+# maxima(value_df,plotx="dist")
+# maxima(value_df,"eTNFa")
+# maxima(value_df,"activated_frac")
+# 
+# ##Kymographs
+# kymograph(value_df)
+# kymograph(value_df,"eTNFa")
+# 
+# ###AUCS
+# auc_plot(value_df)
+# auc_plot(value_df,"eTNFa")
+# 
+# ### Response times
+# response_plot(value_df)
+# response_plot(value_df,"eTNFa")
 
 
 
 #### Multiple ####
-multiple <- list(
-  c("tmax",55),
-  c("base",1)
-)
-
-plots <- list()
-for (i in 1:(length(multiple))) {
-  par <- multiple[[i]][1]
-  runs <- multiple[[i]][2]
-  print(runs)
-  
-  dfs <- load(par,as.double(runs))
-  plots[[as.character(i)]] <- all_cells(dfs)
-}
-
-plots[[2]] + plots[[1]]
+# multiple <- list(
+#   c("tmax",56),
+#   c("tmax",57),
+#   c("tmax",58),
+#   c("base",1)
+# )
+# 
+# plots <- list()
+# for (i in 1:(length(multiple))) {
+#   par <- multiple[[i]][1]
+#   runs <- multiple[[i]][2]
+#   print(runs)
+#   
+#   dfs <- load(par,as.double(runs))
+#   plots[[as.character(i)]] <- all_cells(dfs,ploty="NFKB.n")
+# }
+# 
+# 
+# ggarrange(plots[[1]],plots[[2]],plots[[3]],plots[[4]],ncol=2,nrow =2,common.legend = FALSE,labels=c("σ²=0.001","σ²=0.01","σ²=0.1","Base"),
+# label.x = 0.5,font.label=list(size=10,face="plain"))
+# 
+# ggsave("compare_TNFa2.png",path="compares",width=3000, height=2000, units="px")
 
 ### "Recovery time" of NFKB
 # co <- lm(time ~ log(tmax), as.data.frame(value_df |> group_by(cell.id) |> filter(time > 100) |> filter(NFKB.n < 1e-5) |> slice_min(time)))$coefficients
@@ -116,56 +165,56 @@ plots[[2]] + plots[[1]]
 
 
 #### TRYING OUT ####
-x <- 0:800
-y1 <- x
-y2 <- x/2
-y3 <- x/3
-y4 <- x^(1/2)
-plot(x,y4, type = 'l')
-lines(x,y4)
-
-
-df <- data.frame("time" = c(x), "cell.id" = rep(c(1),each=801), "NFKB.n" = c(x^(3)))
-all_response_times(df)
-plot(df[["time"]],df[["NFKB.n"]], type='l')
-abline(v=599.9997)
-abline(h=0)
-abline(v=800)
-abline(v=0)
-abline(h=(800)^(3))
-599.9997*(800)^(3)
-(800)^(3)*800-auc((x)^(3))
-auc(sqrt(x)[1:266])
-
-
-source("functions.R")
-df <- data.frame("time" = c(0:10), "cell.id" = rep(c(1),each=11), "NFKB.n" = c(0,0,0,0,0,0,0,2,2,2,2))
-all_response_times(df,t=10)
-
-plot(df[["time"]],df[["NFKB.n"]])
-
-x <- seq(0,0.3,0.001)
-y <- rlnorm(10000,10,0.1)
-plot(x,dlnorm(x,log(0.01),0.001))
-hist((rlnorm(10000,log(0.01),0.001)))
-sd(log(rlnorm(10000,log(0.01),(3))))
-mean(log(rlnorm(10000,(0.01),log(3))))
-hist(y)
-hist(log(y))
-mean(log(y))
-sd(log(y))
-
-m <- 0.01
-vari <- (0.5)
-y2 <- rlnorm(100000,log(m^2/(sqrt(m^2+vari))),sqrt(log(1+(vari/(m^2)))))
-max(y2)
-h <- (hist(y2,probability = TRUE, breaks=30))
-plot(y2,y2)
-hist(log(y2))
-mean(log(y2))
-var(log(y2))
-sd(log(y2))
-mean(y2)
-var(y2)
-sd(y2)
-plot((seq(0,0.01,0.00001)),plnorm(seq(0,0.01,0.00001),log(m^2/(sqrt(m^2+v))),sqrt(log(1+(v/m^2)))))
+# x <- 0:800
+# y1 <- x
+# y2 <- x/2
+# y3 <- x/3
+# y4 <- x^(1/2)
+# plot(x,y4, type = 'l')
+# lines(x,y4)
+# 
+# 
+# df <- data.frame("time" = c(x), "cell.id" = rep(c(1),each=801), "NFKB.n" = c(x^(3)))
+# all_response_times(df)
+# plot(df[["time"]],df[["NFKB.n"]], type='l')
+# abline(v=599.9997)
+# abline(h=0)
+# abline(v=800)
+# abline(v=0)
+# abline(h=(800)^(3))
+# 599.9997*(800)^(3)
+# (800)^(3)*800-auc((x)^(3))
+# auc(sqrt(x)[1:266])
+# 
+# 
+# source("functions.R")
+# df <- data.frame("time" = c(0:10), "cell.id" = rep(c(1),each=11), "NFKB.n" = c(0,0,0,0,0,0,0,2,2,2,2))
+# all_response_times(df,t=10)
+# 
+# plot(df[["time"]],df[["NFKB.n"]])
+# 
+# x <- seq(0,0.3,0.001)
+# y <- rlnorm(10000,10,0.1)
+# plot(x,dlnorm(x,log(0.01),0.001))
+# hist((rlnorm(10000,log(0.01),0.001)))
+# sd(log(rlnorm(10000,log(0.01),(3))))
+# mean(log(rlnorm(10000,(0.01),log(3))))
+# hist(y)
+# hist(log(y))
+# mean(log(y))
+# sd(log(y))
+# 
+# m <- 0.01
+# vari <- (0.5)
+# y2 <- rlnorm(100000,log(m^2/(sqrt(m^2+vari))),sqrt(log(1+(vari/(m^2)))))
+# max(y2)
+# h <- (hist(y2,probability = TRUE, breaks=30))
+# plot(y2,y2)
+# hist(log(y2))
+# mean(log(y2))
+# var(log(y2))
+# sd(log(y2))
+# mean(y2)
+# var(y2)
+# sd(y2)
+# plot((seq(0,0.01,0.00001)),plnorm(seq(0,0.01,0.00001),log(m^2/(sqrt(m^2+v))),sqrt(log(1+(v/m^2)))))
